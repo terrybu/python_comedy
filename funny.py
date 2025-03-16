e7e0c3da8302dc7f4ad9affaa24cc5b7a5d033bf
Terry Bu <terrybu88@gmail.com>
2025-03-16 09:49:58 -0700

    updates

---
+++ funny.py
@@ -0,0 +1,13 @@
+savings_account = 100 #money I have left in dollars
+hope = 1000
+friends = 2
+
+while savings_account > 0 and hope > 1:
+	print("I'm still alive and gonna make it in comedy")
+	savings_account = savings_account - 1
+	hope = hope - 1
+
+def keep_going(hope,friends):
+	print(f"Do not lose {hope}. You still have {friends}.")
+
+print("hey, i'd love to merge my master branch with your private respository")
--- python_joke_post_2025.py
+++ python_joke_post_2025.py
@@ -1,6 +1,6 @@
 open_mics_that_are_gone_now_i_miss_them = ["comedy_blvd", "PDA_Altadena", "ice_house", "dat_phan_and_friends"]
 open_mics_i_still_love = ["fourth_wall", "THC", "Flappers", "Flashback", "the_crow", "no_bad_mics"]
-new_mics_i_love = [ "hot_lunch_mic", "courtyard_comedy", "Chatterbox", "Clubhouse","tao_comedy_studio"]
+new_mics_i_love = ["tao_comedy_studio", "hot_lunch_mic", "courtyard_comedy", "Chatterbox", "Clubhouse"]
 open_mics_where_parking_situation_got_worse = ["badger_and_jam", "hollywood_improv", "comedy_store"]
 
 money_left_in_bank = 20
@@ -15,6 +15,7 @@ def kill_on_stage(authenticity, word_economy, pathos, relatability, presence, tr
 		haters += 1
 	elif bad_set:
 		print("Ain't No Thang")
+		break 
 	return good_set
 
 while new_fans + old_fans > haters_getting_triggered and money_left_in_bank > 0:
